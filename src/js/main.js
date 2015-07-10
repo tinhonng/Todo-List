7238cdb835d065e4a260407e95b339557abc0faf
tinhonngTesting <tin315@hotmail.com>
2015-07-09 17:26:26 -0700

    implemented ListContainer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by tinhonng on 7/9/15.
  */
+var React = require('react');
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <h1>hello</h1>
+        )
+    }
+});
+
+React.render(<App />, document.getElementById('main'));
